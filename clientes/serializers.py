331fdab9b2dcddffca09bd1f6dea76b7ba9e64b0
Gilmar <gilmar.jesus@gmail.com>
2022-03-22 05:41:34 -0300

    Separado a validacao dos campos no artefato validators.

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from clientes.models import Cliente
+from clientes.validators import *
 
 
 class ClienteSerializer(serializers.ModelSerializer):
@@ -7,22 +8,16 @@ class ClienteSerializer(serializers.ModelSerializer):
         model = Cliente
         fields = '__all__'
 
-    def validate_cpf(self, cpf):
-        if len(cpf) != 11:
-            raise serializers.ValidationError('O CPF deve ter 11 dígitos.')
-        return cpf
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf': 'O CPF deve ter 11 dígitos.'})
 
-    def validate_nome(self, nome):
-        if not nome.isalpha():
-            raise serializers.ValidationError('Não inclua números neste campo.')
-        return nome
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome': 'Não inclua números neste campo.'})
 
-    def validate_rg(self, rg):
-        if len(rg) != 9:
-            raise serializers.ValidationError('O RG deve ter 9 dígitos.')
-        return rg
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg':'O RG deve ter 9 dígitos.'})
 
-    def validate_celular(self, celular):
-        if len(celular) < 11:
-            raise serializers.ValidationError('O celular deve ter no mínimo 11 dígitos.')
-        return celular
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular':'O celular deve ter no mínimo 11 dígitos.'})
+        return data
